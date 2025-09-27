f750e300ba25b7a038a5e70e03cb9b3441b49567
John M. Drescher <drescherjm@gmail.com>
2025-09-27 01:39:18 -0400

    Add some missing vcpkg configuration for Linux

---
+++ vcpkg/triplets/x64-linux.cmake
@@ -0,0 +1,6 @@
+# ~/git/custom-triplets/x64-linux-dynamic.cmake
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic) # This changed from static to dynamic
+
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
